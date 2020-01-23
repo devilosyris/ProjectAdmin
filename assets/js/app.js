e3adb88cb72ebbb95102afa7fc7603f93bc51e6e
Your Name <ci.rios@laposte.net>
2020-01-23 13:14:29 +0100

    REACT

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,8 +4,30 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 
 var $ = require('jquery');
+
+const App = () => {
+    return (
+        <HashRouter>
+            <Navbar />
+            <main className="container pt-5">
+                <Switch>
+                    <Route path="/" component={HomePage} />
+                </Switch>
+            </main>
+        </HashRouter>
+    );
+};
+
+const rootElement = document.querySelector('#app');
+ReactDOM.render(<App />, rootElement);
+
 require('popper.js');
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.scss');
